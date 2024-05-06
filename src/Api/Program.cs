8d83ccf8ccee7584a80b15d14699e8a3887a1fc4
phuelmedoabarca <p.huelmedo@outlook.com>
2024-05-06 19:06:45 -0400

    cambios program

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -57,11 +57,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.ConfigureExceptionHandler();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
